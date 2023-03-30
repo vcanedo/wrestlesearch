34ed956598044fba72928910fb36755f835b49a6
victor canedo <victorcanedo1@hotmail.com>
2023-03-30 12:54:17 -0300

    added validations to all models

--- app/models/reign.rb
+++ app/models/reign.rb
@@ -1,4 +1,6 @@
 class Reign < ApplicationRecord
+  validates :duration, presence: true
+
   belongs_to :wrestler
   belongs_to :title
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  validates :nickname, presence: true, uniqueness: true, length: { minimum: 6 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
