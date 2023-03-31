f34d51d55806a79aeb0912db08f0091934b8da35
victor canedo <victorcanedo1@hotmail.com>
2023-03-31 11:00:19 -0300

    controllers action edited

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -1,2 +1,19 @@
 class TitlesController < ApplicationController
+  before_action :set_title, only: %i[show]
+
+  def index
+    @titles = Title.all
+  end
+
+  def show; end
+
+  private
+
+  def set_title
+    @title = Title.find(params[:id])
+  end
+
+  # def title_params
+  #   params.require(:title).permit(:name, :image, :time_used)
+  # end
 end
--- app/controllers/wrestlers_controller.rb
+++ app/controllers/wrestlers_controller.rb
@@ -1,2 +1,19 @@
 class WrestlersController < ApplicationController
+  before_action :set_wrestler, only: %i[show]
+
+  def index
+    @wrestlers = Wrestler.all
+  end
+
+  def show; end
+
+  private
+
+  # def wrestler_params
+  #   params.require(:wrestler).permit(:name, :image, :youtube_url)
+  # end
+
+  def set_wrestler
+    @wrestler = Wrestler.find(params[:id])
+  end
 end
